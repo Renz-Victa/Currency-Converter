c431dc39b74f4fd63526fd2630f1313e9944316d
Renz Victa <victarenz472@gmail.com>
2026-03-01 08:53:12 +0200

    Edit navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useMemo } from 'react';
 
 export const navbar = () => {
-
+    return (
+        <nav className="navbar">
+            <ul>
+                <li className="nav-item">
+                    <a href="#">Dashboard</a>
+                </li>
+            </ul>
+        </nav>
+    )
 };
 
 export default navbar;
